9deb369d2d5812ee91cb327eaa5467a4a08f38fd
Adam Jamiu <adamjamiu98@gmail.com>
2022-10-22 16:47:40 +0100

    Navbar Theme icon and color mode toggle icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import logo from '../src/Cryptocurrency.png';
 import './App.css';
 import AppBar from './components/navbar/Appbar';
 
 function App() {
+  const [modalStatus, setModalStatus] = useState(false);
+  const [coinId, setCurrenCoinid] = useState("");
+  const [themeLight, setTheme] = useState(true);
+
+  const onsetThemeHandler = () => {
+    setTheme((status) => !status);
+  };
+
+  const onCloseModalHandler = (id) => {
+    setModalStatus((status) => !status);
+    if (!id) {
+      setCurrenCoinid("");
+    } else {
+      setCurrenCoinid(id);
+    }
+  };
+
   return (
-    <div className="App">
-      <AppBar props={logo} />
+    <div className={`${themeLight === true ? "" : "bg-[#0a1929] text-white"}`}>
+      <AppBar themeStatus={themeLight} onSetTheme={onsetThemeHandler} />
     </div>
   );
 }
