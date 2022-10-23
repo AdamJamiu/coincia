f350cc945009cc4b451419c8d73078d48d6f2cc7
Adam Jamiu <adamjamiu98@gmail.com>
2022-10-23 07:29:56 +0100

    Making the app Progressive

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,11 @@ root.render(
     <App />
   </ThemeProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
